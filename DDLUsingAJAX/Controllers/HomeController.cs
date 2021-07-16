3f9fbd8bd872031f41e1846b2e0c2947b01fa1e0
navreen1999 <72292938+navreen1999@users.noreply.github.com>
2021-07-16 11:35:03 +0530

    Task

---
+++ DDLUsingAJAX/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DDLUsingAJAX.Models;
+
+namespace DDLUsingAJAX.Controllers
+{
+    public class HomeController : Controller
+    {
+        public dbCountryEntities entity = new dbCountryEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Country = entity.Countries.ToList();
+           
+            return View();
+        }
+       [HttpGet]
+        public JsonResult GetStates(int Country_id)
+        {
+            var states = entity.States.Where(s => s.Country_id == Country_id).Select
+                (s => new
+                {
+                    Id = s.State_id,
+                    StateName = s.State_name
+                }).ToList();
+
+
+            return Json(new { msg = "success", data = states },JsonRequestBehavior.AllowGet);
+                        
+
+        }
+        [HttpGet]
+        public JsonResult GetCity(int State_id)
+        {
+            var city = entity.Cities.Where(s => s.State_id == State_id).Select(
+                s => new
+                {
+                    City_id = s.City_id,
+                    City_Name = s.City_name
+                }
+                );
+            return Json(new { msg = "success", data = city }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DDLUsingAJAX/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DDLUsingAJAX")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DDLUsingAJAX")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("26d63bb8-16be-4dbc-8633-00fc4a6ece7f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
